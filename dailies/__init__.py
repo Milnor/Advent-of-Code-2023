0cc02fd753334495e389bbd89d6e0024ece2f953
Milnor <7789866+Milnor@users.noreply.github.com>
2023-12-02 02:34:54 -0500

    day 2 both parts, correct first draft

--- dailies/__init__.py
+++ dailies/__init__.py
@@ -1,3 +1,4 @@
 from .day01 import part_one, part_two
+from .day02 import part_one, part_two
 
 #__all__ = ["part_one", "part_two"]
--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Load daily challenges and display results. """
 
-from dailies import day01
+from dailies import day01, day02
 
 
 def import_challenge_data(path):
@@ -20,8 +20,16 @@ def main():
 
     print("--- Day 1: Trebuchet?! ---")
     print(f"Part One: {one}")
-    print(f"Part Two: {two}")
+    print(f"Part Two: {two}\n")
 
+    challenge_data = import_challenge_data("data/day02.txt")
+
+    one = day02.part_one(challenge_data)  # correct answer was 2416
+    two = day02.part_two(challenge_data)  # correct answer was TBD
+
+    print("--- Day 2: Cube Conundrum ---")
+    print(f"Part One: {one}")
+    print(f"Part Two: {two}\n")
 
 if __name__ == "__main__":
     main()
